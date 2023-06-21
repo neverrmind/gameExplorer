a5f68947c885277fb08a6da7be736dbdd2b55ecc
Özge Canlı <ozge.canli@halkbank.com.tr>
2023-06-22 00:10:31 +0300

    Detail screen designed

--- app/src/main/java/com/example/gamesapplication/data/remote/model/GameDetailListModel.kt
+++ app/src/main/java/com/example/gamesapplication/data/remote/model/GameDetailListModel.kt
@@ -6,4 +6,9 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class GameDetailListModel(
     @Json(name = "id") val id: Int?,
+    @Json(name = "name") val name: String? = "",
+    @Json(name = "background_image") val background_image: String? = "",
+    @Json(name = "description_raw") val description_raw: String? = "",
+    @Json(name = "reddit_url") val reddit_url: String? = "",
+    @Json(name = "website") val website: String? = "",
 )
--- app/src/main/java/com/example/gamesapplication/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/gamesapplication/ui/detail/DetailScreen.kt
@@ -1,14 +1,163 @@
 package com.example.gamesapplication.ui.detail
 
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
 
 @Composable
 fun DetailScreen(name: String?) {
     val detailViewModel: DetailViewModel = hiltViewModel()
     LaunchedEffect(detailViewModel.gameId.collectAsState().value) {
         detailViewModel.getGameDetail(name)
+    }
+    val gameDetail by detailViewModel.gameDetailModel.collectAsState()
+    val image = rememberImagePainter(data = gameDetail?.background_image)
+    val name = gameDetail?.name
+    val description = gameDetail?.description_raw
+    val reddit_url = gameDetail?.reddit_url
+    val website = gameDetail?.website
+    val isClicked = remember { mutableStateOf(false) }
+    val isClickedWeb = remember { mutableStateOf(false) }
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box {
+            Image(
+                painter = image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth().height(275.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+
+            ) {
+                if (name != null) {
+                    Text(
+                        text = name,
+                        modifier = Modifier
+                            .padding(5.dp),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 36.sp,
+                        letterSpacing = 0.38.sp,
+                        textAlign = TextAlign.Right,
+                        color = Color(0xFFFFFFFF),
+                    )
+                }
+            }
+        }
+        Box(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column {
+                Text(
+                    text = "Game Description",
+                    modifier = Modifier
+                        .padding(5.dp),
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 17.sp,
+                    letterSpacing = 0.41.sp,
+                    textAlign = TextAlign.Right,
+                )
+                if (description != null) {
+                    Text(
+                        text = description,
+                        modifier = Modifier
+                            .padding(8.dp),
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 10.sp,
+                        letterSpacing = 0.41.sp,
+                        textAlign = TextAlign.Left,
+                        maxLines = 4,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+        }
+        Divider(
+            color = Color.LightGray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Box(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                modifier = Modifier
+                    .clickable {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(reddit_url))
+                        launcher.launch(intent)
+                        isClicked.value = true
+                    },
+                text = "Visit reddit",
+                fontWeight = FontWeight.Normal,
+                fontSize = 17.sp,
+                letterSpacing = 0.41.sp,
+                textAlign = TextAlign.Right,
+                color = if (isClicked.value) Color.Blue else Color.Black,
+            )
+        }
+        Divider(
+            color = Color.LightGray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Box(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                modifier = Modifier
+                    .clickable {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(website))
+                        launcher.launch(intent)
+                        isClickedWeb.value = true
+                    },
+                text = "Visit website",
+                fontWeight = FontWeight.Normal,
+                fontSize = 17.sp,
+                letterSpacing = 0.41.sp,
+                textAlign = TextAlign.Right,
+                color = if (isClickedWeb.value) Color.Blue else Color.Black,
+
+                )
+        }
+        Divider(
+            color = Color.LightGray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
     }
 }
